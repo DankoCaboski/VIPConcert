25c226133f612ee9e0933a31ba13e6c56f7ebe40
danko caboski <caboskidanko@gmail.com>
2023-11-30 10:58:17 -0300

    fix: error 404 firefox favicon

--- demo/src/main/java/com/bertoti/demo/services/AuthorizationService.java
+++ demo/src/main/java/com/bertoti/demo/services/AuthorizationService.java
@@ -15,7 +15,7 @@ public class AuthorizationService implements UserDetailsService{
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByUsername(username);
+        User user = userRepository.findByName(username);
         if (user == null) {
             throw new UsernameNotFoundException("User not found");
         }
